a2a6c5ded280b878f0621d1c93c40c6ec61dd842
moumi123 <duttamoumita23@gmail.com>
2018-09-01 23:28:50 +0530

    p/e ratio changes

--- pe_ratio_details.py
+++ pe_ratio_details.py
@@ -14,24 +14,13 @@ sql='select name,url from sector_wise_company;'
 
 df2=pd.read_sql(sql,db)
 
-sql="Drop table if exists pe_bucket;"
-cursor.execute(sql)
-sql = """
-CREATE TABLE pe_bucket (Company_Name  CHAR(255),Pe_standalone CHAR(200),Pe_consolidated CHAR(100),Pe_standalone_bucket  CHAR(200),Pe_consolidated_bucket CHAR(100))
-        """
-cursor.execute(sql)
-print "Table pe_bucket created successfully"
-sql="""Insert into pe_bucket (Company_Name,
-         Pe_standalone,
-        Pe_consolidated,
-         Pe_standalone_bucket,Pe_consolidated_bucket) values (%s,%s,%s,%s,%s)"""
 
-
-#print df2
 all_info=[]
 bucket_array=np.linspace(0,699,num=700/5)
 #print type(bucket_array)
-for info in range(0,1):
+# n denotes the number of companies, here just for sample I have taken 100
+n=100
+for info in range(0,n):
     #len(df2['name'])
     url=df2.iloc[info][1]
     company_name=df2.iloc[info][0]
@@ -53,26 +42,17 @@ for info in range(0,1):
         if col_name == 'P/E' and count==1:
             #print "P/E  Standalone:" + (attr.find('div',{'class':"FR gD_12"})).text
             count=2
-            l.append((attr.find('div',{'class':"FR gD_12"})).text)
+            l.append(((attr.find('div',{'class':"FR gD_12"})).text).encode('ascii','ignore'))
         elif col_name == 'P/E' and count==2:
             #print "P/E  Consolidated:" + (attr.find('div',{'class':"FR gD_12"})).text
             count=1
-            l.append((attr.find('div',{'class':"FR gD_12"})).text)
+            l.append(((attr.find('div',{'class':"FR gD_12"})).text).encode('ascii','ignore'))
             break
-    try:
-        l.append(float(l[1].encode('ascii','ignore')))
-    except:
-        l.append(l[1])
-    try:
-        l.append(float(l[2].encode('ascii','ignore')))
-    except:
-        l.append(l[2])
-    cursor.execute(sql,tuple(l))
-        
-#all_info.append(l)
+          
+    all_info.append(l)
 
 
-'''df=pd.DataFrame(all_info,columns=['Company Name','P/E  Standalone','P/E  Consolidated'])
+df=pd.DataFrame(all_info,columns=['Company Name','P/E  Standalone','P/E  Consolidated'])
 
 stand = []
 conso=[]
@@ -89,18 +69,8 @@ for x in df['P/E  Consolidated']:
         conso.append(0)
 
 
-#print np.array(map (lambda x: float(x.encode('ascii','ignore')) if x.encode('ascii','ignore')!= '-' else x,df['P/E  Standalone']))
-print pd.cut([15],bucket_array)
-
-
 df['P/E  Standalone Bucket'] = pd.cut(np.array(stand),bucket_array)
 df['P/E  Consolidated Bucket'] = pd.cut(np.array(conso), bucket_array)
-print df
-'''
-sql="select * from pe_bucket"
-print pd.read_sql(sql,db)
+print df.groupby('P/E  Standalone Bucket')['Company Name'].apply(list)
 
-
-
-
-
+print df.groupby('P/E  Consolidated Bucket')['Company Name'].apply(list)
